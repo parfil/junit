1ffb524ddc81ad0748dd4076252ca2f57df51ba2
Shailesh Sandeep Patil <124047221+parfil@users.noreply.github.com>
2023-04-25 15:55:32 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,18 @@
+package com.junit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    public int add(int a,int b) {
+    	return a+b;
+    	
+    }
+   
+}
